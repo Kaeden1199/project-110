eb10d9de56b71dd4f7b8835758f3ee9a7eac7d3d
Kaeden1199 <76447743+Kaeden1199@users.noreply.github.com>
2021-05-10 14:26:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,71 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+ 
+var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox");
+
+function start()
+{
+    Textbox.innerHTML = "";
+    recognition.start();
+}
+ 
+recognition.onresult = function(event) {
+
+ console.log(event);
+
+var Content = event.results[0][0].transcript;
+
+    Textbox.innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your Selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_selfie();
+        save();
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
 Webcam.set({
-  width: 350,
-  height: 300,
-  image_format: 'png',
-  png_quality: 90
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
 });
-camera= document.getElementById("camera");
-Webcam.attach('#camera');
-function take_snapshot(){
-  Webcam.snap(function(data_uri){
-      document.getElementById("result").innerHTML='<img id="captured_image"src="'+ data_uri + '"/>';
-  });
-  
 
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
 }
-console.log('ml5 version:', ml5.version);
-classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8noc9-G70/model.json',modelLoaded);
-function modelLoaded(){
-  console.log(' model Loaded ');
 
-}
-function check() { img = document.getElementById('captured_image'); 
-classifier.classify(img, gotResult); }
-  
 
-function gotResult(error, results) { if (error) { console.error(error); } else {
-  console.log(results);
-   document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
 
-}
-}
